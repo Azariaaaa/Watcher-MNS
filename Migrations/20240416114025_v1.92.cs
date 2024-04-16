c30db941e5d2803c7fa95a8b750204fd9233c500
Louis K <71547475+Macbeth57@users.noreply.github.com>
2024-04-16 14:35:28 +0200

    [Updated] visual modifications / cleaned navbar

---
+++ Migrations/20240416114025_v1.92.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WatchMNS.Migrations
+{
+    /// <inheritdoc />
+    public partial class v192 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "PostCode",
+                keyValue: null,
+                column: "PostCode",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PostCode",
+                table: "Client",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "NativeCountry",
+                keyValue: null,
+                column: "NativeCountry",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NativeCountry",
+                table: "Client",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "NativeCity",
+                keyValue: null,
+                column: "NativeCity",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NativeCity",
+                table: "Client",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "Country",
+                keyValue: null,
+                column: "Country",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Client",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Client",
+                keyColumn: "City",
+                keyValue: null,
+                column: "City",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Client",
+                type: "longtext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Client",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PostCode",
+                table: "Client",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NativeCountry",
+                table: "Client",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "NativeCity",
+                table: "Client",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Client",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "City",
+                table: "Client",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "BirthDate",
+                table: "Client",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+    }
+}
--- Models/Client.cs
+++ Models/Client.cs
@@ -14,13 +14,13 @@ namespace WatchMNS.Models
         [Required]
         public string Email { get; set; }
         [Required]
-        public string? Address { get; set; }
-        public string? PostCode { get; set; }
-        public string? City { get; set; }
-        public string? Country { get; set; }
-        public DateTime? BirthDate { get; set; }
-        public string? NativeCity { get; set; }
-        public string? NativeCountry { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string NativeCity { get; set; }
+        public string NativeCountry { get; set; }
         public ProfessionnalStatus? ProfessionnalStatus { get; set; }
         public Role? Role { get; set; }
         public List<Training>? Trainings { get; set; } = new List<Training>();
