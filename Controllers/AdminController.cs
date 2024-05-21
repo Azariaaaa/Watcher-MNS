923c77a940223e43af1bd8292daaf6de5ebad751
Louis K <71547475+Macbeth57@users.noreply.github.com>
2024-05-21 22:12:21 +0200

    [Added] Feature that give the possibility for admins to edit user roles - WIP

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WatchMNS.Database;
+using WatchMNS.Models;
+using WatchMNS.ViewModel;
+
+namespace WatchMNS.Controllers
+{
+    public class AdminController : Controller
+    {
+        private DatabaseContext _dbContext = new DatabaseContext();
+        private UserManager<Client> _userManager { get; set; }
+
+
+        public AdminController(UserManager<Client> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> ManageRole(string id)
+        {
+            AdminEditRoleViewModel viewModel = new AdminEditRoleViewModel();
+
+            Client? client = _dbContext.Client
+                .Where(client => client.Id == id)
+                .FirstOrDefault();
+
+            viewModel.Client = client;
+
+            if (client != null)
+            {
+                var clientRole = await _userManager
+                .GetRolesAsync(client);
+
+                viewModel.Role = clientRole;
+            }
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ ViewModel/AdminEditRoleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WatchMNS.Models;
+
+namespace WatchMNS.ViewModel
+{
+    public class AdminEditRoleViewModel
+    {
+        public Client? Client { get; set; }
+        public IList<string>? Role { get; set; }
+
+    }
+}
