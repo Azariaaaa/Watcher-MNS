d48060d772f2b88190a61d1aca80af6c1b4c8e33
Louis K <71547475+Macbeth57@users.noreply.github.com>
2024-05-22 16:54:34 +0200

    [Added] Feature that give the possibility for admins to edit user profiles

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using WatchMNS.Database;
 using WatchMNS.Models;
 using WatchMNS.ViewModel;
@@ -47,16 +48,44 @@ namespace WatchMNS.Controllers
         [HttpPost]
         public async Task<IActionResult> ManageRole(AdminEditRoleViewModel viewModel)
         {
-            Client client = await _userManager.FindByIdAsync(viewModel.ClientId);
-            IList<string> CurrentUserRole = await _userManager.GetRolesAsync(client);
-            await _userManager.RemoveFromRolesAsync(client, CurrentUserRole);
-            await _userManager.AddToRoleAsync(client, viewModel.NewRoleName);
-            Console.WriteLine("viewModel.Client.Id" + viewModel.ClientId);
-            Console.WriteLine("Current user role : " + CurrentUserRole[0]);
-            Console.WriteLine("New role name : " + viewModel.NewRoleName);
+            if(viewModel.ClientId != null)
+            {
+                Client client = await _userManager.FindByIdAsync(viewModel.ClientId);
+                IList<string> CurrentUserRole = await _userManager.GetRolesAsync(client);
+                await _userManager.RemoveFromRolesAsync(client, CurrentUserRole);
+                await _userManager.AddToRoleAsync(client, viewModel.NewRoleName);
+            }
 
             return RedirectToAction("AdminPanel", "AdminPanel");
         }
 
+        public async Task<IActionResult> AdminEditUser(string id)
+        {
+
+            Client client = await _userManager.FindByIdAsync(id);
+
+            List<ProfessionnalStatus> professionnalStatusesList = new List<ProfessionnalStatus>();
+            professionnalStatusesList =  _dbContext.ProfessionnalStatus.ToList();
+
+            AdminEditUserViewModel viewModel = new AdminEditUserViewModel
+            {
+                Id = id,
+                Firstname = client.Firstname,
+                Lastname = client.Lastname,
+                Email = client.Email,
+                Address = client.Address,
+                PostCode = client.PostCode,
+                City = client.City,
+                Country = client.Country,
+                BirthDate = client.BirthDate,
+                PhoneNumber = client.PhoneNumber,
+                NativeCity = client.NativeCity,
+                NativeCountry = client.NativeCountry,
+                ProfessionnalStatusId = client.ProfessionnalStatusId,
+                professionnalStatuses = professionnalStatusesList
+            };
+
+            return View(viewModel);
+        }
     }
 }
---
+++ ViewModel/AdminEditUserViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WatchMNS.Models;
+
+namespace WatchMNS.ViewModel
+{
+    public class AdminEditUserViewModel
+    {
+        public string Id { get; set; }
+        [Required]
+        public string Firstname { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string? Address { get; set; }
+        public string? PostCode { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? NativeCity { get; set; }
+        public string? NativeCountry { get; set; }
+        public int? ProfessionnalStatusId { get; set; }
+        public List<ProfessionnalStatus>? professionnalStatuses { get; set; }
+    }
+}
