90567e4fc66e5a7982bed56c8a361cae29e839db
Louis K <71547475+Macbeth57@users.noreply.github.com>
2024-06-03 12:45:04 +0200

    [Added] MIME mapping class

---
+++ MimeMapping.cs
@@ -0,0 +1,19 @@
+﻿namespace WatchMNS
+{
+    public static class MimeMapping
+    {
+        private static readonly Dictionary<string, string> Mappings = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+    {
+        {".pdf", "application/pdf"},
+        {".jpg", "image/jpeg"},
+        {".jpeg", "image/jpeg"},
+        {".png", "image/png"},
+    };
+
+        public static string GetMimeMapping(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            return extension != null && Mappings.ContainsKey(extension) ? Mappings[extension] : "application/octet-stream";
+        }
+    }
+}
